799d76ebcf4e7358ca92741996df7e4e1a3557cf
Bandita Das <140870941+BanditaDas@users.noreply.github.com>
2024-04-20 21:51:06 +0530

    3  updated

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -4,7 +4,15 @@ import React, { createContext, useEffect, useState } from 'react'
 export const productContext =  createContext()
 
 function Context(props) {
-    const [products, setproducts] = useState(JSON.parse(localStorage.getItem("products")) || null);
+    // const [products, setproducts] = useState(JSON.parse(localStorage.getItem("products")) || null);
+
+    localValue = localStorage.getItem("products")
+if (!localValue) { 
+    localStorage.setItem([])
+    localValue = "[]"
+}
+
+const [products, setproducts] = useState( JSON.parse(localValue) )
 
     // const getProducts = async ()=>{
     //     try{
